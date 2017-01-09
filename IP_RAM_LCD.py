9811c051f9c15c4d27b487ce223444d30298e952
Extreme-Machine <monish1apr@gmail.com>
2017-01-09 05:37:06 +0000

    Clean Up

--- IP_RAM_LCD.py
+++ IP_RAM_LCD.py
@@ -4,7 +4,6 @@
 import time
 import Adafruit_CharLCD as LCD
 
-
 # Raspberry Pi pin configuration:
 lcd_rs        = 26  # Note this might need to be changed to 21 for older revision Pi's.
 lcd_en        = 19
@@ -37,27 +36,9 @@ lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,
 
 # Print a two line message
 #lcd.message('MK\nExtreme-Machine')
-
 # Wait 5 seconds
 #time.sleep(5.0)
 
-# Demo showing the cursor.
-#lcd.clear()
-#lcd.show_cursor(True)
-#lcd.message('Message')
-
-#time.sleep(5.0)
-
-# Demo showing the blinking cursor.
-#lcd.clear()
-#lcd.blink(True)
-#lcd.message('Blink cursor')
-
-#time.sleep(5.0)
-
-# Stop blinking and showing cursor.
-#lcd.show_cursor(False)
-#lcd.blink(False)
 
 # Demo scrolling message right/left.
 #lcd.clear()
@@ -69,22 +50,6 @@ lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,
 #for i in range(lcd_columns-len(message)):
 #    time.sleep(0.5)
 #    lcd.move_left()
-
-# Demo turning backlight off and on.
-#lcd.clear()
-#lcd.message('Flash backlight\nin 1 seconds...')
-#time.sleep(1.0)
-# Turn backlight off.
-#lcd.set_backlight(0)
-#time.sleep(2.0)
-# Change message.
-#lcd.clear()
-#lcd.message('Goodbye!')
-# Turn backlight on.
-#lcd.set_backlight(1)
-
-# .. more script
-# .. more script
 
 # Return RAM information (unit=kb) in a list                                       
 # Index 0: total RAM                                                               
